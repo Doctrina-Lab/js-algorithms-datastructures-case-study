b56aee8287c6847cf575c69bb3d5d68ce7ee237f
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-02-10 07:34:49 +0200

    Refactor valid-anagram.js

--- js/valid-anagram.js
+++ js/valid-anagram.js
@@ -2,19 +2,21 @@ function validAnagram(str1, str2) {
     if (str1.length !== str2.length) {
         return false
     }
-    let frequencyCounter1 = {}
-    let frequencyCounter2 = {}
-    for (let val of str1) {
-        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+
+    let lookup = {}
+
+    for (let letter of str1) {
+        lookup[letter] = (lookup[letter] || 0) + 1
     }
-    for (let val of str2) {
-        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
-    }
-    for (let key in frequencyCounter1) {
-        if (frequencyCounter1[key] !== frequencyCounter2[key]) {
+
+    for (let letter of str2) {
+        if (!lookup[letter]) {
             return false
+        } else {
+            lookup[letter] -= 1
         }
     }
+
     return true
 }
 
