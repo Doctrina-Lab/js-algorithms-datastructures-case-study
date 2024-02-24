294c8fde4602ccc1d9d94869ecee44aba4de40ed
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-02-24 12:58:38 +0200

    Add more tests for 'maxSubarraySum'

--- js/problem-solving-patterns/max-subarray-sum.test.js
+++ js/problem-solving-patterns/max-subarray-sum.test.js
@@ -16,4 +16,19 @@ describe('maxSubArraySum', () => {
     it('returns 13 for ([4, 2, 1, 6, 2], 4)', () => {
         expect(maxSubarraySum([4, 2, 1, 6, 2], 4)).toEqual(13)
     })
+    it('returns 700 for ([100, 200, 300, 400], 2)', () => {
+        expect(maxSubarraySum([100, 200, 300, 400], 2)).toEqual(700)
+    })
+    it('returns 39 for ([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)', () => {
+        expect(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)).toEqual(39)
+    })
+    it('returns 5 for ([-3, 4, 0, -2, 6, -1], 2)', () => {
+        expect(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)).toEqual(5)
+    })
+    it('returns 5 for ([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)', () => {
+        expect(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)).toEqual(5)
+    })
+    it('returns null for ([2, 3], 3)', () => {
+        expect(maxSubarraySum([2, 3], 3)).toEqual(null)
+    })
 })
