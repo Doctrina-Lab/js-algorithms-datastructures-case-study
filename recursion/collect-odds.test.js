f053b354f3117cb422af87c7a0c897ced2f5c0a9
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-04-01 14:13:07 +0300

    Add collect-odds.js

---
+++ recursion/collect-odds.test.js
@@ -0,0 +1,7 @@
+const collectOdds = require('./collect-odds')
+
+describe('collectOdds', () => {
+    it('returns [1, 3, 5, 7, 9] for [1, 2, 3, 4, 5, 6, 7, 8, 9]', () => {
+        expect(collectOdds([1, 2, 3, 4, 5, 6, 7, 8, 9])).toEqual([1, 3, 5, 7, 9])
+    })
+})
