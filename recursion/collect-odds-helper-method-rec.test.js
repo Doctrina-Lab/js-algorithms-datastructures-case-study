b6a5590e1eb55fb33d46d2e4181d99ad2a92cd2e
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-04-02 07:18:08 +0300

    Rename collect-odds to collect-odds-helper-method-rec

--- recursion/collect-odds-helper-method-rec.test.js
+++ recursion/collect-odds-helper-method-rec.test.js
@@ -1,4 +1,4 @@
-const collectOdds = require('./collect-odds')
+const collectOdds = require('./collect-odds-helper-method-rec')
 
 describe('collectOdds', () => {
     it('returns [1, 3, 5, 7, 9] for [1, 2, 3, 4, 5, 6, 7, 8, 9]', () => {
