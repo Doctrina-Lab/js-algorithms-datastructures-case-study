e0515e5e0a115349940b1d11d62bfbb72a3590c8
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-10-14 15:42:37 +0300

    Add optimization to the bubbleSort

--- sorting-algorithms/bubble-sort.js
+++ sorting-algorithms/bubble-sort.js
@@ -1,12 +1,16 @@
 function bubbleSort(arr) {
+    let noSwaps
     for (let last = arr.length - 1; last > 0; last--) {
+        noSwaps = true
         for (let current = 0; current < last; current++) {
             if (arr[current] > arr[current + 1]) {
                 let temp = arr[current]
                 arr[current] = arr[current + 1]
                 arr[current + 1] = temp
+                noSwaps = false
             }
         }
+        if (noSwaps) break
     }
     return arr
 }
