e0e23afe7d8f712aae20859275a10fa22225c101
Vova Myroniuk <vladimironiuk@gmail.com>
2025-01-16 08:29:13 +0200

    DoublyLinkedList creation creats an empty list

--- data-structures/doubly-linked-list/doubly-linked-list.test.js
+++ data-structures/doubly-linked-list/doubly-linked-list.test.js
@@ -1,7 +1,11 @@
 const DoublyLinkedList = require('./doubly-linked-list')
 
-describe('DoublyLinkedList.push(value)', () => {
-  it('just scaffolding', () => {
-    expect(true).toEqual(true)
+describe('DoublyLinkedList creation', () => {
+  it('creates an empty list', () => {
+    let list = new DoublyLinkedList()
+    expect(list.head).toEqual(null)
+    expect(list.tail).toEqual(null)
+    expect(list.length).toEqual(0)
   })
 })
+
