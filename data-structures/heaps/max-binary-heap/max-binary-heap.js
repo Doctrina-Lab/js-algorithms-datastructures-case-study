9576c61aa1fb7e2a9fc7c038c7e1ec382e6b4a8d
Vova Myroniuk <vladimironiuk@gmail.com>
2025-10-07 16:02:58 +0300

    feat: implement MaxBinaryHeap.extractMax

--- data-structures/heaps/max-binary-heap/max-binary-heap.js
+++ data-structures/heaps/max-binary-heap/max-binary-heap.js
@@ -26,10 +26,85 @@ class MaxBinaryHeap {
       this.values[0] = this.values[this.values.length - 1]
       this.values[this.values.length - 1] = temp
     }
+
+    const bubbleDown = () => {
+      let parentIndex = 0
+      const parentElement = this.values[parentIndex]
+
+      const swapParentAndChild = (childIndex, childElement) => {
+        this.values[parentIndex] = childElement
+        this.values[childIndex] = parentElement
+        parentIndex = childIndex
+      }
+
+      while (true) {
+        const leftChildIndex = 2 * parentIndex + 1
+        const rightChildIndex = 2 * parentIndex + 2
+        const length = this.values.length
+
+        // parent does not have children (quit)
+        if (leftChildIndex >= length && rightChildIndex >= length) {
+          return
+        }
+
+        // parent has both children
+        if (leftChildIndex < length && rightChildIndex < length) {
+          const leftChildElement = this.values[leftChildIndex]
+          const rightChildElement = this.values[rightChildIndex]
+
+          // parent has both children and both children are smaller than parent (quit)
+          if (leftChildElement < parentElement && rightChildElement < parentElement) {
+            return
+          }
+
+          // parent has both children and both children are greater than parent
+          if (leftChildElement > parentElement && rightChildElement > parentElement) {
+            // left is greater than right (swap)
+            if (leftChildElement > rightChildElement) {
+              swapParentAndChild(leftChildIndex, leftChildElement)
+            } // right is greater than left (swap)
+            else {
+              swapParentAndChild(rightChildIndex, rightChildElement)
+            }
+          } // parent has both children and left child is greater than parent (swap)
+          else if (leftChildElement > parentElement) {
+            swapParentAndChild(leftChildIndex, leftChildElement)
+          } // parent has both children and right child is greater than parent
+          else {
+            swapParentAndChild(rightChildIndex, rightChildElement)
+          }
+        } // parent has left child
+        else if (leftChildIndex < length) {
+          const leftChildElement = this.values[leftChildIndex]
+
+          // parent has left child and it's smaller than or equal to parent (quit)
+          if (leftChildElement <= parentElement) {
+            return
+          } // parent has left child and it's greater than parent (swap)
+          else {
+            swapParentAndChild(leftChildIndex, leftChildElement)
+          }
+        } // parent has right child
+        else if (rightChildIndex < length) {
+          const rightChildElement = this.values[rightChildIndex]
+
+          // parent has right child and it's smaller than or equal to parent (quit)
+          if (rightChildElement <= parentElement) {
+            return
+          } // parent has right child and it's greater than parent
+          else {
+            swapParentAndChild(rightChildIndex, rightChildElement)
+          }
+        }
+      }
+    }
+
     if (this.values.length === 0) { return null }
     if (this.values.length === 1) { return this.values.pop() }
+
     swapFirstAndLast()
     let maxValue = this.values.pop()
+    bubbleDown()
     return maxValue
   }
 }
