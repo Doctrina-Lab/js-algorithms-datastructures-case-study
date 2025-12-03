9e8e0d430aee0766af34b471d8a559aef9c3711d
Vova Myroniuk <vladimironiuk@gmail.com>
2025-12-03 12:51:57 +0200

    refactor: use previous implementation of priority queue

--- data-structures/graph/weighted-graph.js
+++ data-structures/graph/weighted-graph.js
@@ -1,21 +1,4 @@
-class PriorityQueue {
-  constructor() {
-    this.values = []
-  }
-
-  enqueue(value, priority) {
-    this.values.push({value, priority})
-    this.sort()
-  }
-
-  dequeue() {
-    return this.values.shift()
-  }
-
-  sort() {
-    this.values.sort((a, b) => a.priority - b.priority)
-  }
-}
+const PriorityQueue = require('../priority-queue/priority-queue')
 
 class WeightedGraph {
   constructor() {
